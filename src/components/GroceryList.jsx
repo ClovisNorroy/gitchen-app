ecb797a6a53ddc9fab115f027a3d011d94dc8fb2
ClovisNorroy <clovis.norroy@gmail.com>
2024-05-21 13:53:15 +0200

    prettyfied

--- src/components/GroceryList.jsx
+++ src/components/GroceryList.jsx
@@ -1,65 +1,76 @@
 import { CheckBoxOutlineBlank } from "@mui/icons-material";
-import { List, ListItem, ListItemButton, ListItemIcon, TextField } from "@mui/material";
+import {
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  TextField,
+} from "@mui/material";
 import { useRef, useState } from "react";
 
-export default function GroceryList(){
+export default function GroceryList() {
+  const TEST_DATA = ["Pommes", "Gateaux", "Oeufs", "PTT", "Fromage", "Poulet"];
+  const [groceryList, setGroceryList] = useState(TEST_DATA);
+  const newItemRef = useRef();
 
-    const TEST_DATA = ["Pommes", "Gateaux", "Oeufs", "PTT", "Fromage", "Poulet"];
-    const [groceryList, setGroceryList] = useState(TEST_DATA);
-    const newItemRef = useRef();
+  function handleToggleChecked(itemIndex) {
+    const updatedGroceryList = [...groceryList.toSpliced(itemIndex, 1)];
+    setGroceryList(updatedGroceryList);
+  }
 
-    function handleToggleChecked(itemIndex){
-        const updatedGroceryList = [...groceryList.toSpliced(itemIndex, 1)];
-        setGroceryList(updatedGroceryList);
+  function handleItemChange(newValue, index) {
+    const updatedGroceryList = [
+      ...groceryList.map((groceryItem, groceryIndex) => {
+        if (groceryIndex === index) {
+          return newValue;
+        } else return groceryItem;
+      }),
+    ];
+    setGroceryList(updatedGroceryList);
+  }
+
+  function handleNewItemKeyUp(event) {
+    if (event.key === "Enter") {
+      setGroceryList([...groceryList, newItemRef.current.value]);
+      newItemRef.current.value = "";
     }
+  }
 
-    function handleItemChange(newValue, index){
-        const updatedGroceryList = [...groceryList.map( (groceryItem, groceryIndex) => {
-            if(groceryIndex === index){
-                return newValue;
-            }
-            else
-                return groceryItem;
-        })];
-        setGroceryList(updatedGroceryList);
-    }
-
-    function handleNewItemKeyUp(event){
-        if(event.key === "Enter"){
-            setGroceryList([...groceryList, newItemRef.current.value]);
-            newItemRef.current.value = "";
-        }
-    }
-
-    return (
-        <List>
-            {groceryList.map( (item, index) => (
-                <ListItem key={`item-${index}`}>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <CheckBoxOutlineBlank onClick={() => handleToggleChecked(index)}/>
-                        </ListItemIcon>
-                        <TextField
-                            variant="standard"
-                            InputProps={{disableUnderline: true}}
-                            value={item}
-                            onChange={(event) => {handleItemChange(event.target.value, index)}}/>
-                    </ListItemButton>
-                </ListItem>
-            ))}
-            <ListItem key='new-item'>
-                <ListItemButton>
-                    <ListItemIcon>
-                        <CheckBoxOutlineBlank/>
-                    </ListItemIcon>
-                    <TextField
-                        variant="standard"
-                        InputProps={{disableUnderline: true}}
-                        inputRef={newItemRef}
-                        onKeyUp={handleNewItemKeyUp}
-                        placeholder="Ajouter un objet"/>
-                </ListItemButton>
-            </ListItem>
-        </List>
-    )
-};
+  return (
+    <List>
+      {groceryList.map((item, index) => (
+        <ListItem key={`item-${index}`}>
+          <ListItemButton>
+            <ListItemIcon>
+              <CheckBoxOutlineBlank
+                onClick={() => handleToggleChecked(index)}
+              />
+            </ListItemIcon>
+            <TextField
+              variant="standard"
+              InputProps={{ disableUnderline: true }}
+              value={item}
+              onChange={(event) => {
+                handleItemChange(event.target.value, index);
+              }}
+            />
+          </ListItemButton>
+        </ListItem>
+      ))}
+      <ListItem key="new-item">
+        <ListItemButton>
+          <ListItemIcon>
+            <CheckBoxOutlineBlank />
+          </ListItemIcon>
+          <TextField
+            variant="standard"
+            InputProps={{ disableUnderline: true }}
+            inputRef={newItemRef}
+            onKeyUp={handleNewItemKeyUp}
+            placeholder="Ajouter un objet"
+          />
+        </ListItemButton>
+      </ListItem>
+    </List>
+  );
+}
