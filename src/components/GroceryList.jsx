d6f16b22a76a424f664d7bcaccf089233c210da9
ClovisNorroy <clovis.norroy@gmail.com>
2024-05-21 16:01:45 +0200

    Add list reordering

--- src/components/GroceryList.jsx
+++ src/components/GroceryList.jsx
@@ -1,17 +1,43 @@
-import { CheckBoxOutlineBlank } from "@mui/icons-material";
+import { CheckBoxOutlineBlank } from '@mui/icons-material';
+import SwapVertIcon from '@mui/icons-material/SwapVert';
 import {
+    IconButton,
   List,
   ListItem,
   ListItemButton,
   ListItemIcon,
   TextField,
+  Typography,
 } from "@mui/material";
 import { useRef, useState } from "react";
+
+//TODO: !! Use react-dnd !!
+const moveItem = (array, to, from) => {
+    const item = array[from];
+    array.splice(from, 1);
+    array.splice(to, 0, item);
+    return array;
+};
 
 export default function GroceryList() {
   const TEST_DATA = ["Pommes", "Gateaux", "Oeufs", "PTT", "Fromage", "Poulet"];
   const [groceryList, setGroceryList] = useState(TEST_DATA);
+  const [isMoving, setIsMoving] = useState(false);
+  const [isMovingtoIndex, setIsMovingToIndex] = useState(null);
+  const [whatItemIsMoved, setWhatItemIsMoved] = useState({item:null, index:null});
   const newItemRef = useRef();
+
+  function handleChangeItemIndex(newIndex, oldIndex){
+    const oldList = [...groceryList];
+    const newList = moveItem(oldList, newIndex, oldIndex);
+    setGroceryList(newList);
+    setIsMoving(false);
+  }
+
+  function handleMoveItem(movedItem){
+    setWhatItemIsMoved(movedItem);
+    setIsMoving(true);
+  }
 
   function handleToggleChecked(itemIndex) {
     const updatedGroceryList = [...groceryList.toSpliced(itemIndex, 1)];
@@ -37,9 +63,26 @@ export default function GroceryList() {
   }
 
   return (
-    <List>
+    <List dense>
       {groceryList.map((item, index) => (
-        <ListItem key={`item-${index}`}>
+        <>
+                <ListItem
+        key={`list-move-${index}`}
+            sx={{display: isMoving ? "block" : "none", padding: 1}}
+            onMouseEnter={() => setIsMovingToIndex(index)}
+            onMouseLeave={() => setIsMovingToIndex(null)}
+            onClick={() => handleChangeItemIndex(index, whatItemIsMoved.index)}>
+                <Typography>{isMovingtoIndex === index ? whatItemIsMoved.item : ''}</Typography>
+        </ListItem>
+        <ListItem key={`item-${index}`}
+            secondaryAction={
+                <IconButton edge="end" 
+                    aria-label='move-button'
+                    onClick={() => handleMoveItem({item: item, index: index})}
+                    sx={{display: isMoving ? "none" : "block"}}>
+                    <SwapVertIcon/>
+                </IconButton>
+            }>
           <ListItemButton>
             <ListItemIcon>
               <CheckBoxOutlineBlank
@@ -56,6 +99,7 @@ export default function GroceryList() {
             />
           </ListItemButton>
         </ListItem>
+        </>
       ))}
       <ListItem key="new-item">
         <ListItemButton>
