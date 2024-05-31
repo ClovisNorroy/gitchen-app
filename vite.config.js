545474ccb558ef530b24fdcda6e2576a3f7a1e64
ClovisNorroy <clovis.norroy@gmail.com>
2024-05-31 17:28:07 +0200

    Add emotion suppport

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react()]
+  plugins: [react({
+    jsxImportSource: '@emotion/react',
+    babel: {
+      plugins: ['@emotion/babel-plugin'],
+    },
+  })]
 })
