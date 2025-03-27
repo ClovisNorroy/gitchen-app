4c676a75b7519ab65359cc65a848c52c5e14a6e1
ClovisNorroy <clovis.norroy@gmail.com>
2025-03-27 16:29:54 +0100

    Improve RecipeCard by matching menu droppable size when dragging

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -4,13 +4,15 @@ import { Link } from "react-router-dom";
 import { CSS } from "@dnd-kit/utilities";
 
 export default function RecipeCard({name, image, id, ingredients, recipeid, nolink}){
-    const {attributes, listeners, setNodeRef, transform} = useDraggable({
+    const {attributes, listeners, setNodeRef, transform, isDragging} = useDraggable({
         id: id,
         data: {ingredients: ingredients, name: name}
     });
 
     const style = {
         transform: CSS.Translate.toString(transform),
+        height: isDragging ? 79 : 330,
+        width: isDragging ? 181 : 225
       };
 
     return(
@@ -18,14 +20,14 @@ export default function RecipeCard({name, image, id, ingredients, recipeid, noli
             style={style}
             {...listeners}
             {...attributes}
-            sx={{width: 225, marginRight: 3, height: 330}}
+            sx={{marginRight: 3}}
         >
           <Link to={`/recipe/${recipeid}`} style={{pointerEvents: nolink ? 'none' : 'auto'}}>
-                <CardMedia
+                { !isDragging && <CardMedia
                 sx={{ height: 225, width: 225}}
                     image={`data:image/png;base64, ${image}`}
                     title={name}
-                />
+                />}
                 <CardContent>
                     <Typography
                     sx={{textWrap: 'wrap'}} align='center'>{name}</Typography>
